5c2fa40cafe1feb51af7e34a946b67cf4f62fd93
RominMadaliya <rominskillqode@gmail.com>
2024-06-06 17:36:47 +0530

    upadate

---
+++ 1_datatype_variable.dart
@@ -0,0 +1,15 @@
+import 'dart:io';
+void main()
+{
+    int a=10,b=20;
+    String name="hello";
+    bool isflase=false;
+    double number=4.5;
+
+    print(a);
+    print(b);
+    print(name);
+    print(isflase);
+    print(number);
+
+}    
---
+++ data_outinput.dart
@@ -0,0 +1,18 @@
+import 'dart:io';
+void main()
+{
+    int a=10,b=20;
+    String name="hello";
+    bool isflase=false;
+    double number=4.5;
+
+    //print(a);
+    //print(b);
+    print(name);
+    print(isflase);
+    print(number);
+    int? a=int.parse(stdin.readLineSync()!).'iostring';
+    print(a);
+    int? b=int.parse(stdin.readLineSync()!).'iostring';
+    print(b);
+} 

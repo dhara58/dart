212d0e7cd848f820cc4aeb1cd33c8a875d9bb1a8
RominMadaliya <rominskillqode@gmail.com>
2024-06-13 17:25:23 +0530

    update

---
+++ const.dart
@@ -0,0 +1,16 @@
+import 'dart:ffi';
+
+void main()
+{
+  const float=3.5;
+  print(float);
+
+  const Float1=2.3;
+  print(Float1);
+
+  const String earth='sunrise';
+  print(earth);
+
+  const Char='M';
+  print(Char);
+}
--- final.dart
+++ final.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 void main()
 {
   final hello='hello world';
@@ -5,4 +7,10 @@ void main()
 
   final String hello1='hello sunshine';
   print(hello1);
+
+  final Float=5.5;
+  print(Float);
+
+  final int=5;
+  print(int);
 }

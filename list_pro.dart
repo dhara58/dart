32da6c9b6fde353124268fbbf965ab14dd0c00bd
RominMadaliya <rominskillqode@gmail.com>
2024-06-17 17:08:27 +0530

    update

---
+++ list_pro.dart
@@ -0,0 +1,12 @@
+void main()
+{
+  var document=[1,2,3,4,5];
+
+  print(document.first);
+   print(document.isEmpty);
+    print(document.isNotEmpty);
+     print(document.length);
+      print(document.last);
+       print(document.reversed);
+       print(document.single);
+}

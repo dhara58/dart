1e200fb4e2498518d26ae450937f532807f37344
lensi07 <nensianghan07@email.com>
2024-06-20 17:02:36 +0530

    update

--- sets.dart
+++ sets.dart
@@ -2,7 +2,13 @@ import 'dart:ffi';
 
 void main()
 {
-  print("Initializing the Set");  
-  var students=<int>{10,20,30,40,50};
-  print(students);
-}
+  print("insert element values in the Set");  
+  var dis={10,20,30,40,50};
+
+  var data=<int>{};
+  data.add(70);
+  print(data);
+
+  data.addAll(dis);
+  print(data);
+} 
